d03771333f24c8b14b3c6284944183f850fa8139
JohnBonde <jjbonde1@gmail.com>
2020-03-13 15:35:43 -0600

    commit

--- main.js
+++ main.js
@@ -113,3 +113,70 @@ function mirror(arr) {
   let arr2 = arr.slice(0, arr.length - 1).reverse();
   return arr.concat(arr2);
 }
+
+// Create a function to multiply all of the values in an array by the amount of values in the given array.
+
+function multiplyByLength(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    let elem = arr[i];
+    res.push(elem * arr.length);
+  }
+  return res;
+}
+
+// Given a string of letters, how many capital letters are there?
+
+function capitalLetters(str) {
+  let count = 0;
+  let arr = str.split("");
+  arr.forEach(x => {
+    if (x == x.toUpperCase()) count++;
+  });
+  return count;
+}
+
+// The mean of a group of numbers is calculated by summing all numbers, and dividing this sum by the total count of numbers in the group.
+// Given a sorted array of numbers, return the mean (rounded to one decimal place).
+
+function mean(nums) {
+  reducer = (a, c) => a + c;
+  let sum = nums.reduce(reducer) / nums.length;
+  return parseFloat(sum.toFixed(1));
+}
+
+// Given two strings,p1 and p2, return which person drew their gun the fastest. If both are drawn at the same time, return "tie".
+
+function showdown(p1, p2) {
+  let n1 = p1.split("").indexOf("B");
+  let n2 = p2.split("").indexOf("B");
+  if (n1 < n2) {
+    return "p1";
+  } else if (n1 > n2) {
+    return "p2";
+  } else {
+    return "tie";
+  }
+}
+
+// Imagine a circle and two squares: a smaller and a bigger one. For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+// Create a function, that takes an integer (radius of the circle) and returns the difference of the square-areas.
+
+function squareAreasDifference(r) {
+  let s = Math.sqrt(2 * Math.pow(r, 2));
+  let small = Math.pow(s, 2);
+  return Math.round(small);
+}
+
+// Write a function that takes an array of elements and returns only the integers.
+
+function returnOnlyInteger(arr) {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    let elem = arr[i];
+    if (Number.isInteger(elem)) {
+      res.push(elem);
+    }
+  }
+  return res;
+}
